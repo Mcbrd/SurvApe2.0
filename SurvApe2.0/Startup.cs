0ac58d9a4162beb5d2ce689702a20e3d140c6d00
Brad <bradleymcmanus@hotmal.com>
2016-06-22 16:40:18 -0500

    Add project files.

---
+++ SurvApe2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurvApe2._0.Startup))]
+namespace SurvApe2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
